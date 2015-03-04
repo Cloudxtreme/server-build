7942542f418122d0073c5e619cacd48f7e74cef0
danielmerriott <daniel@merriott.net>
2015-03-04 17:58:02 +1300

    Reorder files & create /var/www
    config-host-aws.sh create or symlinks /data which is needed before permissions on /data can be created
    
    /var/www needs to be setup as git needs the www user's homedir to exist

--- 01-build-core-1/14-config-host-data.sh
+++ 01-build-core-1/14-config-host-data.sh
@@ -9,6 +9,11 @@
 sudo chown root:root /data
 sudo chmod 755 /data
 
+# Create /var/www (required so we can setup /var/www/.gitconfig)
+sudo mkdir /var/www
+sudo chown --recursive www-data:www-data /var/www
+sudo chmod --recursive 2775 /data
+
 # Create /backups
 sudo mkdir /backups
 sudo chown --recursive root:backup /backups
