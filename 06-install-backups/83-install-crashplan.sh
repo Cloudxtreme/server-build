4f9f8d62cf3d5e194ef6d1a145425f78e688df45
danielmerriott <daniel@merriott.net>
2015-03-04 18:38:11 +1300

    Path corrections

--- 06-install-backups/83-install-crashplan.sh
+++ 06-install-backups/83-install-crashplan.sh
@@ -12,14 +12,14 @@ curl --progress-bar --location \
      --url "http://download.crashplan.com/installs/linux/install/CrashPlan/CrashPlan_3.7.0_Linux.tgz" \
      --output ~ubuntu/source/temp/crashplan.tar.gz 
 
-tar --gzip --extract --file ~ubuntu/source/temp/crashplan.tar.gz --directory ~/source/temp
+tar --gzip --extract --file ~ubuntu/source/temp/crashplan.tar.gz --directory ~ubuntu/source/temp
 
 # increase the number of files watched by the system
 echo -e "# CrashPlan Service\n# Increase the number of files watched in real time\nfs.inotify.max_user_watches=65000" | sudo tee -a /etc/sysctl.d/60-crashplan.conf
 echo -e "65000" | sudo tee /proc/sys/fs/inotify/max_user_watches
 
 # run the interactive install - no silent option available!
-cd ~/source/temp/CrashPlan-install
+cd ~ubuntu/source/temp/CrashPlan-install
 sudo ./install.sh 
 
 ### once install we will need to connect a client or XTerm to the server to configure CrashPlan
