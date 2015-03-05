22e54f4beaf7e900f6ddeb81f2cd16c66a0b5654
danielmerriott <daniel@merriott.net>
2015-03-05 17:44:32 +1300

    updated backup jobs to be silent
    fixed errors in crontab script
    file renumbering

--- 06-install-backups/01-install-scripts-jobs.sh
+++ 06-install-backups/01-install-scripts-jobs.sh
@@ -19,8 +19,8 @@
 sudo apt-get install rdiff-backup --assume-yes
 
 # copy the scripts
-sudo mkdir /backups/scripts
-sudo cp ~ubuntu/source/config/backups-scripts/* /backups/scripts/
+sudo mkdir /data/scripts
+sudo cp ~ubuntu/source/config/backups-scripts/* /data/scripts/
 
 # setup a crontab file in /etc/crontab.d
 sudo cp ~ubuntu/source/config/backups-conf/mybackups.cron /etc/cron.d/mybackups
