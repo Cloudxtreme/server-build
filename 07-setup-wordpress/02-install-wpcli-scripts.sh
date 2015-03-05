92e50bcda1bec279c49fc87ee709b46f8555c08b
danielmerriott <daniel@merriott.net>
2015-03-05 18:18:23 +1300

    chown www/scripts

--- 07-setup-wordpress/02-install-wpcli-scripts.sh
+++ 07-setup-wordpress/02-install-wpcli-scripts.sh
@@ -8,4 +8,5 @@
 # copy in scripts
 sudo mkdir /data/www/scripts
 sudo cp --recursive ~ubuntu/source/config/wpcli-scripts/* /data/www/scripts
+sudo chown root:www-data /data/www/scripts/*.sh
 sudo chmod a+x /data/www/scripts/*.sh
