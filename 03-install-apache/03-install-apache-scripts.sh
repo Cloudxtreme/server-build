142911ba7004253110a901ef149e27d5d12bc444
danielmerriott <daniel@merriott.net>
2015-03-05 18:18:40 +1300

    chown www/scripts

--- 03-install-apache/03-install-apache-scripts.sh
+++ 03-install-apache/03-install-apache-scripts.sh
@@ -7,4 +7,5 @@
 
 sudo mkdir /data/www/scripts
 sudo cp --recursive ~ubuntu/source/config/apache-scripts/* /data/www/scripts
+sudo chown root:www-data /data/www/scripts/*.sh
 sudo chmod a+x /data/www/scripts/*.sh
